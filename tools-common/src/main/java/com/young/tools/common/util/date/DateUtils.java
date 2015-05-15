c20cc0829eb4827d26ec87c2905eb456d766fa12
yangyong <39901650@qq.com>
2015-05-15 15:40:31 +0800

    增加了时间工具

--- tools-common/src/main/java/com/young/tools/common/util/date/DateUtils.java
+++ tools-common/src/main/java/com/young/tools/common/util/date/DateUtils.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class DateUtils {
 
 	public static String getDateString(Date date,String format){
